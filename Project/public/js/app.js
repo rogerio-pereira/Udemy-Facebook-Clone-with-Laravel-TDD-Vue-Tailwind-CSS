d9ae63508c64bc03d9832c3989de5e022b978546
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-01-08 09:48:02 -0500

    Submitting Post With Image

--- Project/public/js/app.js
+++ Project/public/js/app.js
@@ -1971,6 +1971,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var vuex__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! vuex */ "./node_modules/vuex/dist/vuex.esm.js");
+/* harmony import */ var dropzone__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! dropzone */ "./node_modules/dropzone/dist/dropzone.js");
+/* harmony import */ var dropzone__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(dropzone__WEBPACK_IMPORTED_MODULE_2__);
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
@@ -2008,8 +2010,17 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 //
 
 
+
 /* harmony default export */ __webpack_exports__["default"] = ({
   'name': 'NewPost',
+  data: function data() {
+    return {
+      dropzone: null
+    };
+  },
+  mounted: function mounted() {
+    this.dropzone = new dropzone__WEBPACK_IMPORTED_MODULE_2___default.a(this.$refs.postImage, this.settings);
+  },
   computed: _objectSpread({}, Object(vuex__WEBPACK_IMPORTED_MODULE_1__["mapGetters"])({
     authUser: 'authUser'
   }), {
@@ -2020,8 +2031,41 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       set: lodash__WEBPACK_IMPORTED_MODULE_0___default.a.debounce(function (postMessage) {
         this.$store.commit('updateMessage', postMessage);
       }, 300)
+    },
+    settings: function settings() {
+      var _this = this;
+
+      return {
+        paramName: 'image',
+        url: '/api/posts/',
+        acceptedFiles: 'image/*',
+        clickable: '.dz-clickable',
+        autoProcessQueue: false,
+        params: {
+          'width': 1000,
+          'height': 1000
+        },
+        headers: {
+          'X-CSRF-TOKEN': document.head.querySelector('meta[name=csrf-token]').content
+        },
+        sending: function sending(file, xhr, formData) {
+          formData.append('body', _this.$store.getters.postMessage);
+        },
+        success: function success(event, res) {
+          alert('success');
+        }
+      };
     }
-  })
+  }),
+  methods: {
+    postHandler: function postHandler() {
+      if (this.dropzone.getAcceptedFiles().length) {
+        this.dropzone.processQueue();
+      } else {
+        this.$store.dispatch("postMessage");
+      }
+    }
+  }
 });
 
 /***/ }),
@@ -24481,7 +24525,7 @@ var render = function() {
                     staticClass: "bg-gray-200 ml-2 px-3 py-1 rounded-full",
                     on: {
                       click: function($event) {
-                        return _vm.$store.dispatch("postMessage")
+                        return _vm.postHandler()
                       }
                     }
                   },
@@ -24497,14 +24541,15 @@ var render = function() {
         _c(
           "button",
           {
+            ref: "postImage",
             staticClass:
-              "flex justify-center items-center rounded-full w-10 h-10 bg-gray-200"
+              "dz-clickable flex justify-center items-center rounded-full w-10 h-10 bg-gray-200"
           },
           [
             _c(
               "svg",
               {
-                staticClass: "fill-current w-5 h-5",
+                staticClass: "dz-clickable fill-current w-5 h-5",
                 attrs: {
                   xmlns: "http://www.w3.org/2000/svg",
                   viewBox: "0 0 24 24"
--- Project/resources/js/components/NewPost.vue
+++ Project/resources/js/components/NewPost.vue
@@ -10,7 +10,7 @@
                 <transition name='fade'>
                     <button 
                         v-if="postMessage"
-                        @click='$store.dispatch("postMessage")' 
+                        @click='postHandler()' 
                         class='bg-gray-200 ml-2 px-3 py-1 rounded-full'
                     >
                         Post
@@ -19,8 +19,8 @@
             </div>
 
             <div>
-                <button class='flex justify-center items-center rounded-full w-10 h-10 bg-gray-200'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="fill-current w-5 h-5"><path d="M21.8 4H2.2c-.2 0-.3.2-.3.3v15.3c0 .3.1.4.3.4h19.6c.2 0 .3-.1.3-.3V4.3c0-.1-.1-.3-.3-.3zm-1.6 13.4l-4.4-4.6c0-.1-.1-.1-.2 0l-3.1 2.7-3.9-4.8h-.1s-.1 0-.1.1L3.8 17V6h16.4v11.4zm-4.9-6.8c.9 0 1.6-.7 1.6-1.6 0-.9-.7-1.6-1.6-1.6-.9 0-1.6.7-1.6 1.6.1.9.8 1.6 1.6 1.6z"/></svg>
+                <button ref='postImage' class='dz-clickable flex justify-center items-center rounded-full w-10 h-10 bg-gray-200'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="dz-clickable fill-current w-5 h-5"><path d="M21.8 4H2.2c-.2 0-.3.2-.3.3v15.3c0 .3.1.4.3.4h19.6c.2 0 .3-.1.3-.3V4.3c0-.1-.1-.3-.3-.3zm-1.6 13.4l-4.4-4.6c0-.1-.1-.1-.2 0l-3.1 2.7-3.9-4.8h-.1s-.1 0-.1.1L3.8 17V6h16.4v11.4zm-4.9-6.8c.9 0 1.6-.7 1.6-1.6 0-.9-.7-1.6-1.6-1.6-.9 0-1.6.7-1.6 1.6.1.9.8 1.6 1.6 1.6z"/></svg>
                 </button>
             </div>
         </div>
@@ -30,10 +30,18 @@
 <script>
     import _ from 'lodash';
     import {mapGetters} from 'vuex';
+    import Dropzone from 'dropzone';
 
     export default {
         'name': 'NewPost',
-
+        data: () => {
+            return {
+                dropzone: null,
+            };
+        },
+        mounted() {
+            this.dropzone = new Dropzone(this.$refs.postImage, this.settings);
+        },
         computed: {
             ...mapGetters({
                 authUser: 'authUser',
@@ -45,6 +53,38 @@
                 set: _.debounce(function (postMessage){
                     this.$store.commit('updateMessage', postMessage);
                 }, 300),
+            },
+            settings() {
+                return {
+                    paramName: 'image',
+                    url: '/api/posts/',
+                    acceptedFiles: 'image/*',
+                    clickable: '.dz-clickable',
+                    autoProcessQueue: false,
+                    params: {
+                        'width': 1000,
+                        'height': 1000,
+                    },
+                    headers: {
+                        'X-CSRF-TOKEN': document.head.querySelector('meta[name=csrf-token]').content,
+                    },
+                    sending: (file, xhr, formData) => {
+                        formData.append('body', this.$store.getters.postMessage);
+                    },
+                    success: (event, res) => {
+                        alert('success');
+                    }
+                };
+            }
+        },
+        methods: {
+            postHandler() {
+                if(this.dropzone.getAcceptedFiles().length){
+                    this.dropzone.processQueue();
+                }
+                else {
+                    this.$store.dispatch("postMessage");
+                }
             }
         }
     }
